c4f7a6ea8f88a990d49ecef2e93fe72210ca9902
srv-shivam <shivamsrivastava222001@gmail.com>
2022-02-16 13:42:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/letsguess/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.letsguess
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.customToolBar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.menu_rules -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Clicked on Rules",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return true
+            }
+            R.id.menu_rate_us -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Clicked on Rate Us",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
